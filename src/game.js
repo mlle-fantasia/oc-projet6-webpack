94c136174775ced32626accc0dd4a0a729b247a6
Marina Front <marinafront@hotmail.fr>
2019-10-17 19:27:00 +0200

    infos player

--- src/game.js
+++ src/game.js
@@ -16,8 +16,7 @@ $(document).ready(function() {
 	window.app = new App(players, univers);
 	console.log("app.players", window.app.players);
 	render(window.app.grid);
-	let infoPlayers = renderInfoAllPlayer(window.app.players);
-	$(".info-all-player").append(infoPlayers);
+	renderInfoAllPlayer(window.app.players);
 	//tour par tour
 	nextPlayer();
 });
@@ -33,6 +32,7 @@ function renderYourTurn(player) {
 	$(".info-current-player").append(info);
 	player.showMove(app.grid);
 	render(app.grid);
+	renderInfoAllPlayer(window.app.players);
 	$(".player-" + player.playerNum).addClass("zoom");
 
 	let monCallback = event => {
@@ -117,107 +117,39 @@ function renderObjectCell(cell, newCase) {
 	}
 }
 
-function appendInfoPlayer(players) {
-	for (let p = 0; p < players.length; p++) {
-		const player = players[p];
-		const numPlayer = p + 1;
-		$("#btn-info-player" + numPlayer).show();
-		$("#btn-info-player" + numPlayer).click(() => {
-			if ($("#info-player" + numPlayer).text().length === 0) {
-				let info = renderInfoPlayer(player);
-				$("#info-player" + numPlayer).append(info);
-				$("#info-player" + numPlayer).slideDown();
-			} else {
-				$("#info-player" + numPlayer).empty();
-			}
-		});
-	}
-}
-function renderInfoPlayer(player) {
-	let accessory = "";
-	if (player.accessories[1]) {
-		accessory =
-			`<img src="../images/accessories/` +
-			player.accessories[1].imageGrid +
-			`.png" class="d-block w-100" alt="...">
-		<div>` +
-			player.accessories[1].text +
-			`<br/>
-		avantage : ` +
-			player.accessories[1].avantageText +
-			`<br/>Cet avantage est ` +
-			player.accessories[1].temporality +
-			`<div>`;
-	}
-	//console.log("player.accessories", player.accessories[0]);
-	return (
-		`<div class="info-player  col-12">
-	<div class='info-player-text info-player-titre pt-4'>Joueur ` +
-		player.playerNum +
-		`</div>
-	<div class=" info-player-text info-player-name"> Nom : ` +
-		player.playerName +
-		`</div>
-	<img src="../images/players/img/hero` +
-		player.heroNum +
-		`.jpg" class="d-block w-100" alt="...">
-		<input type="range" class="info-player-text info-player-pt-vie" name="ptVie"
-		min="0" max="` +
-		player.ptVieMax +
-		`" value="` +
-		player.ptVie +
-		`" disabled>
-		<div class="info-player-text">
-		<p> Type : ` +
-		player.type +
-		`</p>
-		<p>Point fort : ` +
-		player.pointFort.text +
-		`</p>
-		</div>
-		<div class="row">
-		<div class="col-6 info-player-accessory">
-		<img src="../images/accessories/` +
-		player.accessories[0].imageGrid +
-		`.png" class="d-block w-100" alt="...">
-		<p>` +
-		player.accessories[0].text +
-		`</p>
-		<p>Dégats : ` +
-		player.accessories[0].degat +
-		`</p>
-
-		</div>
-		<div class="col-6 info-player-accessory">` +
-		accessory +
-		`
-		</div>
-		</div>
-	</div>`
-	);
-}
-
 function renderInfoCurrentPlayer(player) {
 	let accessory = "";
+	let infoAccessory = "";
 	if (player.accessories[1]) {
 		accessory = 'src="images/accessories/' + player.accessories[1].imageGrid + '.png" alt="image accessoire"';
+		let temp = player.accessories[1].temporality === "perpetual" ? "avantage permanent" : "avantage ponctuel";
+		infoAccessory =
+			`<div class="info-name info-accessory accessory-text tolkien">` +
+			player.accessories[1].text +
+			`</div>
+		<div class="info-name info-accessory accessory-avantage">` +
+			player.accessories[1].avantageText +
+			`</div>
+			<div class="info-name info-accessory accessory-temp">` +
+			temp +
+			`</div>`;
 	}
 	let heroSize = $(".info-current-player").width();
 	let ArmorSize = $(".info-current-player").width() / 2;
 	let AccessorySize = $(".info-current-player").width() / 3;
 	return (
 		`<div class="d-flex flex-column cercle-hero">
-	<div class="info-player-name tolkien">` +
+	<div class="info-name tolkien">` +
 		player.playerName +
 		`</div>
 	<div class="background-cercle-player-hero" style="height:` +
 		heroSize +
 		`px">
-		<div class="container-info-playe2-vie-top-right">
+		<div class="container-info-player-vie">
 			<div id="triangle-1"></div>
 			<div id="triangle-2"></div>
 		</div>
-		<div class="info-playe2-vie-top">` +
+		<div class="info-player-vie">` +
 		player.ptVie +
 		`</div>
 		<img class="info-player2-img info-player2-img-hero " src="images/players/img/hero` +
@@ -225,30 +157,49 @@ function renderInfoCurrentPlayer(player) {
 		`.jpg"
 			alt="image hero">
 	</div>
+	<div class="info-name "> type : ` +
+		player.type +
+		`</div>
+	<div class="info-name ">` +
+		player.pointFort.text +
+		`</div>
 </div>
-<div class=" cercle-armor" style="height:` +
+<div class=" container-info-weapon">
+	<div class=" cercle-armor" style="height:` +
 		ArmorSize +
 		`px">
-	<div class="background-cercle-anneau" >
-		<img class="info-player2-img info-player2-img-armor" src="images/accessories/` +
+		<div class="background-cercle-anneau" >
+			<img class="info-player2-img info-player2-img-armor" src="images/accessories/` +
 		player.accessories[0].imageGrid +
 		`.png"
-			alt="image arme">
+				alt="image arme">
+		</div>
 	</div>
+	<div class="info-name info-weapon weapon-text tolkien">` +
+		player.accessories[0].text +
+		`</div>
+	<div class="info-name info-weapon weapon-avantage">` +
+		player.accessories[0].avantageText +
+		`</div>
 </div>
-<div class="cercle-accessory ">
-	<div class="background-cercle-anneau" style="height:` +
+<div class=" container-info-accessory">
+	<div class="cercle-accessory ">
+		<div class="background-cercle-anneau" style="height:` +
 		AccessorySize +
 		`px">
-		<img class="info-player2-img info-player2-img-accessory"` +
+			<img class="info-player2-img info-player2-img-accessory"` +
 		accessory +
 		` >
-	</div>
-
+		</div>
+	</div>` +
+		infoAccessory +
+		`
+	
 </div>`
 	);
 }
 function renderInfoAllPlayer(players) {
+	$(".info-all-players").empty();
 	let widthPlayer = $(".info-all-players").width() / 2;
 	for (let p = 0; p < players.length; p++) {
 		const player = players[p];
@@ -261,6 +212,13 @@ function renderInfoAllPlayer(players) {
 			widthPlayer +
 			`px">
 	<div class="background-cercle-player-hero">
+		<div class="container-info-players-vie">
+			<div id="triangle-1"></div>
+			<div id="triangle-2"></div>
+		</div>
+		<div class="info-players-vie">` +
+			player.ptVie +
+			`</div>
 		<img class="info-player2-img info-player2-img-hero " src="images/players/img/hero` +
 			player.heroNum +
 			`.jpg"
@@ -275,7 +233,7 @@ function renderInfoAllPlayer(players) {
 			accessory +
 			`></div>
 	</div>
-	<div class="info-player-name tolkien">` +
+	<div class="info-name tolkien ">` +
 			player.playerName +
 			`</div>
 </div>`;
